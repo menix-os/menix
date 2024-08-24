5165ab1a634d3ecfe9c93edf5033ad91885d0768
Marvin Friedrich <contact@marvinf.com>
2024-08-24 23:21:51 +0200

    Finish HashMap

--- include/menix/util/hash_map.h
+++ include/menix/util/hash_map.h
@@ -56,9 +56,7 @@ static inline u32 hash(const void* data, usize length)
 		auto __map = map; \
 		/* Allocate buckets */ \
 		if (__map->buckets == NULL) \
-		{ \
 			__map->buckets = kzalloc(__map->capacity * sizeof(*(__map->buckets))); \
-		} \
 		usize __hash = hash(__key, __key_len); \
 		usize __index = __hash % __map->capacity; \
 		auto __bucket = &__map->buckets[__index]; \
@@ -79,3 +77,61 @@ static inline u32 hash(const void* data, usize length)
 		__item->item = (value); \
 		__bucket->count++; \
 	} while (0)
+
+#define hashmap_remove(map, key, key_length) \
+	({ \
+		__label__ __stop; \
+		bool __ok = false; \
+		auto __key_data = (key); \
+		auto __key_len = (key_length); \
+		auto __hashmap = (map); \
+		if (__hashmap->buckets == NULL) \
+			goto __stop; \
+		usize __hash = hash(__key_data, __key_len); \
+		usize __index = __hash % __hashmap->capacity; \
+		auto __bucket = &__hashmap->buckets[__index]; \
+		for (usize __i = 0; __i < __bucket->count; __i++) \
+		{ \
+			if (__key_len != __bucket->items[__i].key_len) \
+				continue; \
+			if (memcmp(__key_data, __bucket->items[__i].key_data, __key_len) == 0) \
+			{ \
+				if (__i != __bucket->count - 1) \
+				{ \
+					memcpy(&__bucket->items[__i], &__bucket->items[__bucket->count - 1], sizeof(*__bucket->items)); \
+				} \
+				__bucket->count -= 1; \
+				__ok = true; \
+				break; \
+			} \
+		} \
+__stop: \
+		__ok; \
+	})
+
+#define hashmap_get(map, ret, key, key_length) \
+	({ \
+		__label__ __stop; \
+		bool __ok = false; \
+		auto __key_data = (key); \
+		auto __key_len = (key_length); \
+		auto __map = (map); \
+		if (__map->buckets == NULL) \
+			goto __stop; \
+		usize __hash = hash(__key_data, __key_len); \
+		usize __index = __hash % __map->capacity; \
+		auto __bucket = &__map->buckets[__index]; \
+		for (usize __i = 0; __i < __bucket->count; __i++) \
+		{ \
+			if (__key_len != __bucket->items[__i].key_len) \
+				continue; \
+			if (memcmp(__key_data, __bucket->items[__i].key_data, __key_len) == 0) \
+			{ \
+				(ret) = __bucket->items[__i].item; \
+				__ok = true; \
+				break; \
+			} \
+		} \
+__stop: \
+		__ok; \
+	})
