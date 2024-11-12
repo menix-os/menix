22d227be743163a2614df7970e9fa8feac9d97e3
Marvin Friedrich <contact@marvinf.com>
2024-11-12 01:09:41 +0100

    toochain: Move vm_map* options to defaults

--- toolchain/arch/defaults.cmake
+++ toolchain/arch/defaults.cmake
@@ -19,6 +19,9 @@ add_option(ktrace_max NUMBER 32)
 # ACPI
 add_option(acpi BOOL ON)
 
+# OpenFirmware
+add_option(open_firmware BOOL OFF)
+
 # Symmetric Multi-processing
 add_option(smp BOOL ON)
 
@@ -30,3 +33,6 @@ add_option(user_stack_base NUMBER 0x70000000000)
 add_option(user_map_base NUMBER 0x7F0000000000)
 add_option(user_interp_base NUMBER 0x60000000000)
 add_option(kernel_stack_size NUMBER 0x200000)
+add_option(vm_map_min_addr NUMBER 0x10000)
+add_option(vm_map_base NUMBER 0xFFFF900000000000)
+add_option(vm_map_foreign_base NUMBER 0xFFFFD00000000000)
--- toolchain/arch/riscv64.cmake
+++ toolchain/arch/riscv64.cmake
@@ -7,7 +7,7 @@ set(MENIX_ARCH_DIR riscv64)
 set(MENIX_ARCH_NAME riscv64)
 
 target_compile_options(common INTERFACE
-	-march=rv64gcv_zihintpause
+	-march=rva22u64
 )
 
 target_compile_options(common_kernel INTERFACE
@@ -15,7 +15,3 @@ target_compile_options(common_kernel INTERFACE
 )
 
 set(max_page_size 0x1000)
-
-add_option(vm_map_min_addr NUMBER 0x10000)
-add_option(vm_map_base NUMBER 0xFFFF900000000000)
-add_option(vm_map_foreign_base NUMBER 0xFFFFD00000000000)
