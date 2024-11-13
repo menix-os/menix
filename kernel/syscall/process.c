7b5707c8d8886252db28f5ef224736fb79e4eba9
Marvin Friedrich <contact@marvinf.com>
2024-11-13 04:28:12 +0100

    proc: Implement chdir syscall

--- kernel/syscall/process.c
+++ kernel/syscall/process.c
@@ -2,13 +2,12 @@
 
 #include <menix/abi/errno.h>
 #include <menix/fs/vfs.h>
+#include <menix/memory/vm.h>
 #include <menix/syscall/syscall.h>
+#include <menix/system/abi.h>
 #include <menix/system/arch.h>
 #include <menix/system/sch/process.h>
 #include <menix/system/sch/scheduler.h>
-
-#include "menix/memory/vm.h"
-#include "menix/system/abi.h"
 
 // Forks a thread by cloning its attributes.
 SYSCALL_IMPL(fork)
@@ -99,3 +98,31 @@ SYSCALL_STUB(getuid)
 
 SYSCALL_STUB(setgid)
 SYSCALL_STUB(getgid)
+
+SYSCALL_IMPL(chdir, VirtAddr path)
+{
+	Process* process = arch_current_cpu()->thread->parent;
+	if (path == 0)
+		return SYSCALL_ERR(EINVAL);
+
+	char* kernel_path = kmalloc(PATH_MAX);
+	vm_user_read(process, kernel_path, path, PATH_MAX);
+
+	VfsNode* new_cwd = vfs_get_node(process->working_dir, kernel_path, true);
+	kfree(kernel_path);
+	if (new_cwd == NULL)
+		return SYSCALL_ERR(ENOENT);
+
+	if (!S_ISDIR(new_cwd->handle->stat.st_mode))
+		return SYSCALL_ERR(ENOTDIR);
+
+	process->working_dir = new_cwd;
+
+	return SYSCALL_OK(0);
+}
+
+SYSCALL_IMPL(waitpid, pid_t pid, VirtAddr status, int flags)
+{
+	// TODO
+	return SYSCALL_OK(0);
+}
