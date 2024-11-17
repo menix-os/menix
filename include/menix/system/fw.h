f440b6f22b99176e833a7b491cf305b4d7fcedba
Marvin Friedrich <contact@marvinf.com>
2024-11-17 23:37:07 +0100

    fw: Document fw_init call order

--- include/menix/system/fw.h
+++ include/menix/system/fw.h
@@ -4,6 +4,7 @@
 
 #include <menix/system/boot.h>
 
+// Initializes firmware. Must be called after `arch_early_init` and before `arch_init`.
 void fw_init(BootInfo* info);
 
 BootInfo* fw_get_boot_info();
