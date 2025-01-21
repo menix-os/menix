63550d86fa835d1d94ed6515332e420c07f8eccc
Marvin Friedrich <contact@marvinf.com>
2025-01-21 23:51:58 +0100

    acpi: Remove redundant functions

--- include/menix/system/acpi/acpi.h
+++ include/menix/system/acpi/acpi.h
@@ -2,15 +2,6 @@
 
 #pragma once
 #include <menix/common.h>
-#include <menix/memory/pm.h>
-
-// Converts an ACPI physical address to a virtual one.
-#define ACPI_ADDR(addr) ((PhysAddr)(addr) + pm_get_phys_base())
 
 // Initializes the ACPI subsystem with a pointer to the RSDP.
 void acpi_init(PhysAddr rsdp);
-
-// Finds a table using its signature.
-// `signature`: A 4-character string with the table's signature.
-// `index`: The nth instance of a table with the given signature.
-void* acpi_find_table(const char* signature, usize index);
