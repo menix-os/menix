c1df6c71171ab071fec11cb7b3f6902af85d5eb6
Marvin Friedrich <contact@marvinf.com>
2025-02-18 16:10:06 +0100

    meta: Fix formatting

--- kernel/system/acpi/uacpi_libc.h
+++ kernel/system/acpi/uacpi_libc.h
@@ -1,15 +1,15 @@
 // Make uACPI use our klibc functions.
 #pragma once
 
-#include <stdint.h>
 #include <stddef.h>
+#include <stdint.h>
 
 void* memcpy(void* restrict dst, const void* restrict src, size_t len);
 void* memmove(void* dst, const void* src, size_t len);
 void* memset(void* dst, int32_t val, size_t len);
 int32_t memcmp(const void* s1, const void* s2, size_t len);
 
-#define uacpi_memcpy memcpy
+#define uacpi_memcpy  memcpy
 #define uacpi_memmove memmove
-#define uacpi_memset memset
-#define uacpi_memcmp memcmp
+#define uacpi_memset  memset
+#define uacpi_memcmp  memcmp
