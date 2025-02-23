d44084ad80365f92ea6b84a440d7a320ed2a93c9
Marvin Friedrich <contact@marvinf.com>
2025-02-23 20:04:50 +0100

    dst: Remove DTB files

--- kernel/system/dst/core.c
+++ kernel/system/dst/core.c
@@ -3,15 +3,18 @@
 #include <menix/common.h>
 #include <menix/system/acpi/acpi.h>
 #include <menix/system/dst/core.h>
+#include <menix/util/cmd.h>
 #include <menix/util/log.h>
 
 void dst_init(BootInfo* info)
 {
-	// TODO
-	// dt_init(info->fdt_blob);
-
-	if (info->acpi_rsdp)
-		acpi_init(info->acpi_rsdp);
+	if (cmd_get_usize("acpi", true))
+	{
+		if (info->acpi_rsdp)
+			acpi_init(info->acpi_rsdp);
+		else
+			print_warn("dst: Unable to configure using ACPI, no RSDP given!\n");
+	}
 	else
-		print_warn("dst: Unable to configure using ACPI, no RSDP given!\n");
+		print_warn("dst: ACPI is disabled!\n");
 }
