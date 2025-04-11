9a71d80f2e480587ccbccde063aa709fdd8d2a56
iretq <32851089+48cf@users.noreply.github.com>
2025-04-11 19:47:24 +0200

    misc: Improve Primitive trait implementation

--- kernel/src/generic/misc/primitive.rs
+++ kernel/src/generic/misc/primitive.rs
@@ -1,31 +1,40 @@
 pub trait Primitive: Copy + Default {
+    type Bytes: Copy;
+
     fn swap_bytes(self) -> Self;
-    fn from_ne_bytes(buf: &[u8]) -> Self {
+    fn from_le_bytes(bytes: Self::Bytes) -> Self;
+    fn from_be_bytes(bytes: Self::Bytes) -> Self;
+    fn to_le_bytes(self) -> Self::Bytes;
+    fn to_be_bytes(self) -> Self::Bytes;
+
+    fn to_ne_bytes(self) -> Self::Bytes {
         #[cfg(target_endian = "little")]
-        return Self::from_le_bytes(buf);
+        return self.to_le_bytes();
         #[cfg(target_endian = "big")]
-        return Self::from_be_bytes(buf);
+        return self.to_be_bytes();
     }
-    fn from_le_bytes(buf: &[u8]) -> Self;
-    fn from_be_bytes(buf: &[u8]) -> Self;
-    fn to_ne_bytes(self, buf: &mut [u8]) {
+
+    fn from_ne_bytes(bytes: Self::Bytes) -> Self {
         #[cfg(target_endian = "little")]
-        self.to_le_bytes(buf);
+        return Self::from_le_bytes(bytes);
         #[cfg(target_endian = "big")]
-        self.to_be_bytes(buf);
+        return Self::from_be_bytes(bytes);
     }
-    fn to_le_bytes(self, buf: &mut [u8]);
-    fn to_be_bytes(self, buf: &mut [u8]);
 }
 
 macro_rules! impl_primitive {
     ($($ty:ty),*) => {
-        $(impl Primitive for $ty {fn swap_bytes(self) -> Self {<$ty>::swap_bytes(self)}
-        fn from_le_bytes(buf: &[u8]) -> Self {<$ty>::from_le_bytes(buf.try_into().unwrap())}
-        fn from_be_bytes(buf: &[u8]) -> Self {<$ty>::from_be_bytes(buf.try_into().unwrap())}
-        fn to_le_bytes(self, buf: &mut [u8]) {self.to_le_bytes(); }
-        fn to_be_bytes(self, buf: &mut [u8]) {self.to_be_bytes(); }
-})*
+        $(
+            impl Primitive for $ty {
+                type Bytes = [u8; core::mem::size_of::<$ty>()];
+
+                fn swap_bytes(self) -> Self { <$ty>::swap_bytes(self) }
+                fn from_le_bytes(bytes: Self::Bytes) -> Self { <$ty>::from_le_bytes(bytes) }
+                fn from_be_bytes(bytes: Self::Bytes) -> Self { <$ty>::from_be_bytes(bytes) }
+                fn to_le_bytes(self) -> Self::Bytes { <$ty>::to_le_bytes(self) }
+                fn to_be_bytes(self) -> Self::Bytes { <$ty>::to_be_bytes(self) }
+            }
+        )*
     };
 }
 
