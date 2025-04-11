f9d73d95aaf0ec9cd620b0eca04967434a52b92f
iretq <32851089+48cf@users.noreply.github.com>
2025-04-11 19:45:02 +0200

    kerne/arch: Use cfg_match!

--- kernel/src/arch/mod.rs
+++ kernel/src/arch/mod.rs
@@ -1,29 +1,32 @@
-#[cfg(target_arch = "x86_64")]
-pub mod x86_64;
-#[cfg(target_arch = "x86_64")]
-mod internal {
-    pub use super::x86_64::*;
-}
+cfg_match! {
+    target_arch = "x86_64" => {
+        pub mod x86_64;
 
-#[cfg(target_arch = "aarch64")]
-pub mod aarch64;
-#[cfg(target_arch = "aarch64")]
-mod internal {
-    pub use super::aarch64::*;
-}
+        mod internal {
+            pub use super::x86_64::*;
+        }
+    }
+    target_arch = "aarch64" => {
+        pub mod aarch64;
 
-#[cfg(target_arch = "riscv64")]
-pub mod riscv64;
-#[cfg(target_arch = "riscv64")]
-mod internal {
-    pub use super::riscv64::*;
-}
+        mod internal {
+            pub use super::aarch64::*;
+        }
+    }
+    target_arch = "riscv64" => {
+        pub mod riscv64;
 
-#[cfg(target_arch = "loongarch64")]
-pub mod loongarch64;
-#[cfg(target_arch = "loongarch64")]
-mod internal {
-    pub use super::loongarch64::*;
+        mod internal {
+            pub use super::riscv64::*;
+        }
+    }
+    target_arch = "loongarch64" => {
+        pub mod loongarch64;
+
+        mod internal {
+            pub use super::loongarch64::*;
+        }
+    }
 }
 
 // Re-export only parts of the architecture implementation that get called by generic code.
