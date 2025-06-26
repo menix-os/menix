b1dddda0a8a1739c75711269a54f17205a33d98a
Marvin Friedrich <contact@marvinf.com>
2025-06-27 00:42:22 +0200

    vfs: Cast flags values to automatic type

--- kernel/src/generic/vfs/fs/mod.rs
+++ kernel/src/generic/vfs/fs/mod.rs
@@ -26,36 +26,36 @@ pub struct Mount {
 bitflags::bitflags! {
     #[derive(Debug)]
     pub struct MountFlags: u32 {
-        const ReadOnly = uapi::MS_RDONLY as u32;
-        const NoSuperUserID = uapi::MS_NOSUID as u32;
-        const NoDev = uapi::MS_NODEV as u32;
-        const NoExec = uapi::MS_NOEXEC as u32;
-        const NoSynchronous = uapi::MS_SYNCHRONOUS as u32;
-        const Remount = uapi::MS_REMOUNT as u32;
-        const MandatoryLock = uapi::MS_MANDLOCK as u32;
-        const DirSync = uapi::MS_DIRSYNC as u32;
-        const NoSymbolFollow = uapi::MS_NOSYMFOLLOW as u32;
-        const NoAccessTime = uapi::MS_NOATIME as u32;
-        const NoDirAccessTime = uapi::MS_NODIRATIME as u32;
-        const Bind = uapi::MS_BIND as u32;
-        const Move = uapi::MS_MOVE as u32;
-        const Rec = uapi::MS_REC as u32;
-        const Silent = uapi::MS_SILENT as u32;
-        const PosixACL = uapi::MS_POSIXACL as u32;
-        const Unbindable = uapi::MS_UNBINDABLE as u32;
-        const Private = uapi::MS_PRIVATE as u32;
-        const Slave = uapi::MS_SLAVE as u32;
-        const Shared = uapi::MS_SHARED as u32;
-        const RelativeTime = uapi::MS_RELATIME as u32;
-        const KernMount = uapi::MS_KERNMOUNT as u32;
-        const IVersion = uapi::MS_I_VERSION as u32;
-        const StrictAccessTime = uapi::MS_STRICTATIME as u32;
-        const LazyTime = uapi::MS_LAZYTIME as u32;
-        const NoRemoteLock = uapi::MS_NOREMOTELOCK as u32;
-        const NoSec = uapi::MS_NOSEC as u32;
-        const Born = uapi::MS_BORN as u32;
-        const Active = uapi::MS_ACTIVE as u32;
-        const NoUser = uapi::MS_NOUSER as u32;
+        const ReadOnly = uapi::MS_RDONLY as _;
+        const NoSuperUserID = uapi::MS_NOSUID as _;
+        const NoDev = uapi::MS_NODEV as _;
+        const NoExec = uapi::MS_NOEXEC as _;
+        const NoSynchronous = uapi::MS_SYNCHRONOUS as _;
+        const Remount = uapi::MS_REMOUNT as _;
+        const MandatoryLock = uapi::MS_MANDLOCK as _;
+        const DirSync = uapi::MS_DIRSYNC as _;
+        const NoSymbolFollow = uapi::MS_NOSYMFOLLOW as _;
+        const NoAccessTime = uapi::MS_NOATIME as _;
+        const NoDirAccessTime = uapi::MS_NODIRATIME as _;
+        const Bind = uapi::MS_BIND as _;
+        const Move = uapi::MS_MOVE as _;
+        const Rec = uapi::MS_REC as _;
+        const Silent = uapi::MS_SILENT as _;
+        const PosixACL = uapi::MS_POSIXACL as _;
+        const Unbindable = uapi::MS_UNBINDABLE as _;
+        const Private = uapi::MS_PRIVATE as _;
+        const Slave = uapi::MS_SLAVE as _;
+        const Shared = uapi::MS_SHARED as _;
+        const RelativeTime = uapi::MS_RELATIME as _;
+        const KernMount = uapi::MS_KERNMOUNT as _;
+        const IVersion = uapi::MS_I_VERSION as _;
+        const StrictAccessTime = uapi::MS_STRICTATIME as _;
+        const LazyTime = uapi::MS_LAZYTIME as _;
+        const NoRemoteLock = uapi::MS_NOREMOTELOCK as _;
+        const NoSec = uapi::MS_NOSEC as _;
+        const Born = uapi::MS_BORN as _;
+        const Active = uapi::MS_ACTIVE as _;
+        const NoUser = uapi::MS_NOUSER as _;
     }
 }
 
