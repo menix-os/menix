68408bd18e9367200c18e25611ad3d07be785302
Marvin Friedrich <contact@marvinf.com>
2025-08-19 12:14:41 +0200

    kernel: Rework ELF header

--- kernel/include/kernel/sys/elf.h
+++ kernel/include/kernel/sys/elf.h
@@ -6,6 +6,18 @@
 
 #define ELF_MAG (const char[4]){0x7F, 'E', 'L', 'F'}
 
+#if defined(__x86_64__) || defined(__aarch64__) || (defined(__riscv) && (__riscv_xlen == 64)) || defined(__loongarch64)
+#define Elf_Hdr  Elf64_Hdr
+#define Elf_Phdr Elf64_Phdr
+#define Elf_Dyn  Elf64_Dyn
+#define Elf_Addr Elf64_Addr
+#define Elf_Off  Elf64_Off
+#define Elf_Nhdr Elf64_Nhdr
+#define Elf_Auxv Elf64_Auxv
+#else
+#error "Unsupported architecture!"
+#endif
+
 enum {
     EI_MAG0 = 0,
     EI_MAG1 = 1,
@@ -127,37 +139,19 @@ enum {
     AT_EGID = 14,
 };
 
-#if __SIZEOF_POINTER__ == 8
-#define Elf_Hdr  Elf64_Hdr
-#define Elf_Phdr Elf64_Phdr
-#define Elf_Dyn  Elf64_Dyn
-#define Elf_Addr Elf64_Addr
-#define Elf_Off  Elf64_Off
-#define Elf_Nhdr Elf64_Nhdr
-#define Elf_Auxv Elf64_Auxv
-#else
-#define Elf_Hdr  Elf32_Hdr
-#define Elf_Phdr Elf32_Phdr
-#define Elf_Dyn  Elf32_Dyn
-#define Elf_Addr Elf32_Addr
-#define Elf_Off  Elf32_Off
-#define Elf_Nhdr Elf32_Nhdr
-#define Elf_Auxv Elf32_Auxv
-#endif
+typedef uint64_t elf64_addr_t;
+typedef uint64_t elf64_off_t;
+typedef uint32_t elf32_addr_t;
+typedef uint32_t elf32_off_t;
 
-typedef uint64_t Elf64_Addr;
-typedef uint64_t Elf64_Off;
-typedef uint32_t Elf32_Addr;
-typedef uint32_t Elf32_Off;
-
-typedef struct {
+struct elf64_ehdr {
     uint8_t e_ident[EI_NIDENT];
     uint16_t e_type;
     uint16_t e_machine;
     uint32_t e_version;
-    Elf64_Addr e_entry;
-    Elf64_Off e_phoff;
-    Elf64_Off e_shoff;
+    elf64_addr_t e_entry;
+    elf64_off_t e_phoff;
+    elf64_off_t e_shoff;
     uint32_t e_flags;
     uint16_t e_ehsize;
     uint16_t e_phentsize;
@@ -165,16 +159,16 @@ typedef struct {
     uint16_t e_shentsize;
     uint16_t e_shnum;
     uint16_t e_shstrndx;
-} Elf64_Ehdr;
+};
 
-typedef struct {
+struct elf32_ehdr {
     uint8_t e_ident[EI_NIDENT];
     uint16_t e_type;
     uint16_t e_machine;
     uint32_t e_version;
-    Elf32_Addr e_entry;
-    Elf32_Off e_phoff;
-    Elf32_Off e_shoff;
+    elf32_addr_t e_entry;
+    elf32_off_t e_phoff;
+    elf32_off_t e_shoff;
     uint32_t e_flags;
     uint16_t e_ehsize;
     uint16_t e_phentsize;
@@ -182,66 +176,66 @@ typedef struct {
     uint16_t e_shentsize;
     uint16_t e_shnum;
     uint16_t e_shstrndx;
-} Elf32_Ehdr;
+};
 
-typedef struct {
+struct elf64_phdr {
     uint32_t p_type;
     uint32_t p_flags;
-    Elf64_Off p_offset;
-    Elf64_Addr p_vaddr;
-    Elf64_Addr p_paddr;
+    elf64_off_t p_offset;
+    elf64_addr_t p_vaddr;
+    elf64_addr_t p_paddr;
     uint64_t p_filesz;
     uint64_t p_memsz;
     uint64_t p_align;
-} Elf64_Phdr;
+};
 
-typedef struct {
+struct elf32_phdr {
     uint32_t p_type;
-    Elf32_Off p_offset;
-    Elf32_Addr p_vaddr;
-    Elf32_Addr p_paddr;
+    elf32_off_t p_offset;
+    elf32_addr_t p_vaddr;
+    elf32_addr_t p_paddr;
     uint32_t p_filesz;
     uint32_t p_memsz;
     uint32_t p_flags;
     uint32_t p_align;
-} Elf32_Phdr;
+};
 
-typedef struct {
+struct elf64_dyn {
     int64_t d_tag;
     union {
         uint64_t d_val;
-        Elf64_Addr d_ptr;
+        elf64_addr_t d_ptr;
     } d_un;
-} Elf64_Dyn;
+};
 
-typedef struct {
+struct elf32_dyn {
     int32_t r_offset;
     union {
         uint32_t d_val;
-        Elf32_Addr d_ptr;
+        elf32_addr_t d_ptr;
     } d_un;
-} Elf32_Dyn;
+};
 
-typedef struct {
+struct elf64_nhdr {
     uint64_t n_namesz;
     uint64_t n_descsz;
     uint64_t n_type;
-} Elf64_Nhdr;
+};
 
-typedef struct {
+struct elf32_nhdr {
     uint32_t n_namesz;
     uint32_t n_descsz;
     uint32_t n_type;
-} Elf32_Nhdr;
+};
 
-typedef struct {
+struct elf64_auxv {
+    uint64_t atype;
+    uint64_t avalue;
+};
+
+struct elf32_auxv {
     uint32_t atype;
     uint32_t avalue;
-} Elf64_Auxv;
-
-typedef struct {
-    uint32_t atype;
-    uint32_t avalue;
-} Elf32_Auxv;
+};
 
 #endif
