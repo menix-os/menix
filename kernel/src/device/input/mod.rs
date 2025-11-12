6a5262430d48cd3ba702271c56fd9e13329fe033
Marvin Friedrich <contact@marvinf.com>
2025-11-12 16:53:04 +0100

    device: Fix missing file

---
+++ kernel/src/device/input/mod.rs
@@ -0,0 +1,56 @@
+use crate::{
+    posix::errno::EResult,
+    vfs::{File, file::FileOps},
+};
+
+use super::CharDevice;
+
+/// Represents a Linux evdev-compatible input device.
+pub struct EventDevice {}
+
+impl CharDevice for EventDevice {
+    fn name(&self) -> &str {
+        "evdev"
+    }
+}
+
+impl FileOps for EventDevice {
+    fn read(&self, file: &File, buffer: &mut [u8], offset: u64) -> EResult<isize> {
+        let _ = (file, buffer, offset);
+        todo!()
+    }
+}
+
+pub struct EventListener {}
+
+impl EventListener {}
+
+#[derive(Clone, Copy)]
+#[repr(C)]
+pub struct InputEvent {
+    pub time: uapi::timeval,
+    pub typ: u16,
+    pub code: u16,
+    pub value: i32,
+}
+static_assert!(size_of::<InputEvent>() == 24);
+
+#[derive(Clone, Copy)]
+#[repr(C)]
+pub struct InputId {
+    pub bustype: u16,
+    pub vendor: u16,
+    pub product: u16,
+    pub version: u16,
+}
+
+#[derive(Clone, Copy)]
+#[repr(C)]
+pub struct InputAbsinfo {
+    pub value: i32,
+    pub minimum: i32,
+    pub maximum: i32,
+    pub fuzz: i32,
+    pub flat: i32,
+    pub resolution: i32,
+}
