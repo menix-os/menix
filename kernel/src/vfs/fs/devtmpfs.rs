442d2229a82c71c78a42d06b9915c8b87953e884
Marvin Friedrich <contact@marvinf.com>
2026-01-02 23:17:07 +0100

    devtmpfs: Add register_device function

--- kernel/src/vfs/fs/devtmpfs.rs
+++ kernel/src/vfs/fs/devtmpfs.rs
@@ -1,7 +1,13 @@
 use crate::{
     posix::errno::EResult,
+    process::Identity,
     util::once::Once,
-    vfs::{Entry, Mount, MountFlags, fs::FileSystem},
+    vfs::{
+        self, Entry, Mount, MountFlags, PathNode,
+        file::FileOps,
+        fs::FileSystem,
+        inode::{Mode, NodeType},
+    },
 };
 use alloc::sync::Arc;
 
@@ -33,4 +39,30 @@ pub fn DEVTMPFS_STAGE() {
         .expect("Unable to create devtmpfs from tmpfs");
 
     unsafe { DEV_MOUNT.init(tmpfs) };
+}
+
+pub fn register_device(
+    name: &[u8],
+    device: Arc<dyn FileOps>,
+    mode: Mode,
+    is_block: bool,
+) -> EResult<()> {
+    let parent = PathNode {
+        mount: DEV_MOUNT.get().clone(),
+        entry: DEV_MOUNT.get().root.clone(),
+    };
+
+    vfs::mknod(
+        parent.clone(),
+        parent.clone(),
+        name,
+        if is_block {
+            NodeType::BlockDevice
+        } else {
+            NodeType::CharacterDevice
+        },
+        mode,
+        Some(device),
+        &Identity::get_kernel(),
+    )
 }
